a6844b3fa01b47d6abb309456b063e8d7790028b
RoseGoldKingVR <114455330+RoseGoldKingVR@users.noreply.github.com>
2026-06-19 10:01:47 -0400

    Create DigitalSum.java
    oh lord

---
+++ DigitalSum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DigitalSum{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while(running){
+            System.out.println("Welcome to the Digital Sum Machine or the DSM trademark pending for short");
+            System.out.println("\nPlease Enter a Number:");
+
+            String input = scanner.nextInt()
+            String test = input.substring(0,4)
+
+
+        }
+
+    }
+
+    //theory 1: all this code is doing is we are inputting some number and its spitting back out a different number. this is a function we need to make a function
+    //test 1:
+
+}
+    //I have to figure out a way to take apart any integer and split it up into a digital sum so 1394 becomes 1+3+9+4
